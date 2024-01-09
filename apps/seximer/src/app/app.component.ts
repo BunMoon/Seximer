5b7b7c8c835866c6c9c2d34edda0f5cf994fc8cf
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-09 20:01:05 +0700

    Setup seximer angular project

--- apps/seximer/src/app/app.component.ts
+++ apps/seximer/src/app/app.component.ts
@@ -1,14 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'seximer-root',
+  imports: [CommonModule, RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'seximer';
+  constructor() {}
 }
